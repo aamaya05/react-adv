6a5985c128ea7aff607c7163faed6ba606d715c5
aamaya <alessandro.amaya5@gmail.com>
2023-10-18 21:39:46 -0600

    feat(lazyload): add lazy load basic ready

---
+++ src/01-lazyload/pages/LazyPage2.tsx
@@ -0,0 +1,7 @@
+export const LazyPage2 = () => {
+  return (
+    <h2>LazyPage1</h2>
+  )
+}
+
+export default LazyPage2;
---
+++ src/01-lazyload/pages/LazyPage3.tsx
@@ -0,0 +1,7 @@
+export const LazyPage3 = () => {
+  return (
+    <h3>LazyPage1</h3>
+  )
+}
+
+export default LazyPage3;
